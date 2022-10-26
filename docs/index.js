2156a240b972322bc002000a118c78945b29c268
Felipe <felipegarciaaffonso@gmail.com>
2022-10-26 13:12:32 -0400

    minimum product working

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,5 @@
-function random_problem () {
-
+function randomProblem() {
+    const prob = ["problem1", "problem2", "problem3"];
+    randomNum = Math.floor(Math.random() * 3);
+    document.getElementById("problems").innerHTML = prob[randomNum];
 }

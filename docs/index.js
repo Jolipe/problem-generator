f6831750975a05d0c4ffc26af3d48990ac2d60e3
Felipe <felipegarciaaffonso@gmail.com>
2022-11-03 14:16:30 -0400

    no idea repetitions

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,4 @@
-url = "https://sheets.googleapis.com/v4/spreadsheets/1Bkr6jAyrwqBDWaUECe6oAPVV1AQ_T9FmGX0LY9rVp34/values/B:B?key" + API_KEY;
-var problems = {};
+url = "https://sheets.googleapis.com/v4/spreadsheets/" + SHEET_ID + "/values/A:A?key=" + API_KEY;
 try {
     var response = {};
 
@@ -8,14 +7,21 @@ try {
         fetchPromise.then((response) => {
                 return response.json();
         }).then((return1) => {
-                problems = return1["values"];
+                problems_list = return1["values"];
         });
 }
 catch(err) {
     console.log(err);
 }
-
 function randomProblem() {
-    randomNum = Math.floor(Math.random() * problems.length) + 2;
-    document.getElementById("problems").innerHTML = problems[randomNum][0];
+    var isNew = true;
+    var newText = "";
+    while (isNew) {
+        randomNum = Math.floor(Math.random() * (problems_list.length - 2)) + 2;
+        newText = problems_list[randomNum][0];
+        if (newText != document.getElementById("problems").innerHTML) {
+            isNew = false;
+        };
+    };
+    document.getElementById("problems").innerHTML = newText;
 }

5c1d607cf477a760fb45044f45821f2e30ed84db
Felipe <felipegarciaaffonso@gmail.com>
2022-11-04 14:53:27 -0400

    backend almost done

--- docs/index.js
+++ docs/index.js
@@ -13,15 +13,45 @@ try {
 catch(err) {
     console.log(err);
 }
+
+var usedProb = ["-"];
+var usedProbNum = 0;
+
 function randomProblem() {
     var isNew = true;
     var newText = "";
     while (isNew) {
         randomNum = Math.floor(Math.random() * (problems_list.length - 2)) + 2;
         newText = problems_list[randomNum][0];
-        if (newText != document.getElementById("problems").innerHTML) {
-            isNew = false;
+        isNew = false;
+        for (let i = 0; i <= usedProb.length - 1; i++) {
+            if (newText == usedProb[i]) {
+                isNew = true;
+            };
         };
     };
+    usedProb.push(newText);
+    usedProbNum = usedProb.length - 1;
+    if (usedProb.length > 20) {
+        usedProb.shift();
+    };
     document.getElementById("problems").innerHTML = newText;
-}
+;}
+
+function handleBack() {
+    if (usedProbNum > 0) {
+        usedProbNum--;
+        document.getElementById("problems").innerHTML = usedProb[usedProbNum];
+    } else {
+        alert("This is the last problem saved.");
+    }
+};
+
+function handleFoward() {
+    if (usedProbNum < usedProb.length - 1) {
+        usedProbNum++;
+        document.getElementById("problems").innerHTML = usedProb[usedProbNum];
+    } else {
+        alert("There are no problems after here.");
+    }
+};
